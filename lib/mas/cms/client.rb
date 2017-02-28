4386895715c037843ff3297106d0f03d0cbe4bde
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-02-28 18:13:29 +0000

    Initial Commit on importing mas cms client to a gem

---
+++ lib/mas/cms/client.rb
@@ -0,0 +1,9 @@
+require "mas/cms/client/version"
+
+module Mas
+  module Cms
+    module Client
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/mas/cms/client/version.rb
@@ -0,0 +1,7 @@
+module Mas
+  module Cms
+    module Client
+      VERSION = "0.1.0"
+    end
+  end
+end
