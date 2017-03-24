dde2c3882daa680ffe976423713a324001806b1c
Jerome Riga <jerome.riga@moneyadviceservice.org.uk>
2017-03-24 10:24:33 +0000

    Bumped `Mas::Cms::Config` test coverage

--- spec/mas/cms/config_spec.rb
+++ spec/mas/cms/config_spec.rb
@@ -1,18 +1,22 @@
 RSpec.describe Mas::Cms::Config do
-  subject(:config) { Mas::Cms::Config.new }
-  let(:timeout)    { 10 }
-  let(:host)       { 'http://localhost:4000' }
+  subject(:config)   { Mas::Cms::Config.new }
+  let(:timeout)      { 10 }
+  let(:open_timeout) { 10 }
+  let(:retries)      { 2 }
+  let(:host)         { 'http://localhost:4000' }
+  let(:api_token)    { 'token' }
 
   before do
-    config.timeout = timeout
-    config.host    = host
+    config.timeout      = timeout
+    config.open_timeout = open_timeout
+    config.host         = host
+    config.api_token    = api_token
+    config.retries      = retries
   end
 
-  it 'responds to `timeout`' do
-    expect(config.timeout).to eq timeout
-  end
-
-  it 'responds to `host`' do
-    expect(config.host).to eq host
+  [:timeout, :open_timeout, :host, :api_token, :retries].each do |attr|
+    it "responds to `#{attr}`" do
+      expect(config.send(attr)).to eq self.send(attr)
+    end
   end
 end
