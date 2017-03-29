a33952e7e36f0eda5cde1f226552f07e4cacee6e
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-03-29 17:04:36 +0100

    Add page feedback creation endpoint
    Page feedback uses a nested resource based on the page rather the inverse,
    so we need to overwrite the path resource method.

--- lib/mas/cms/entity/page_feedback.rb
+++ lib/mas/cms/entity/page_feedback.rb
@@ -2,5 +2,16 @@ module Mas::Cms
   class PageFeedback < Entity
     include Mas::Cms::Resource
     attr_accessor :page_id, :liked, :likes_count, :dislikes_count
+
+    def self.path(slug:, locale:)
+      [
+        api_prefix,
+        locale,
+        slug,
+        resource_name
+      ].compact
+        .join('/')
+        .downcase
+    end
   end
 end
--- spec/mas/cms/entity/page_feedback_spec.rb
+++ spec/mas/cms/entity/page_feedback_spec.rb
@@ -1,5 +1,12 @@
 module Mas::Cms
   RSpec.describe PageFeedback do
     it_has_behavior 'a cms resource entity'
+
+    describe '.path' do
+      subject { described_class.path(locale: 'en', slug: 'personal-pensions') }
+      it 'returns cms route' do
+        expect(subject).to eq('/api/en/personal-pensions/page_feedbacks')
+      end
+    end
   end
 end
